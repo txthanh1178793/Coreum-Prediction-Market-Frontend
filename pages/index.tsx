a6db16f7ad2835c48ccd5ccacc9cbc6106880e01
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-13 17:41:05 +0700

    change import path in Bet.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import WalletLoader from 'components/WalletLoader'
+import CurrentBet from "@/components/CurrentBet";
 import { useSigningClient } from 'contexts/client'
 
 const Home: NextPage = () => {
@@ -37,7 +38,7 @@ const Home: NextPage = () => {
 
 
     // </WalletLoader>
-    <div></div>
+    <CurrentBet></CurrentBet>
   )
 }
 
