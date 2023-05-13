0c5e9ad2e7502e9d6b5fdc96386111a080681225
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-13 17:32:37 +0700

    add css contxt and components

---
+++ components/Bet.tsx
@@ -0,0 +1,230 @@
+import { usePredictStore } from "@/context/PredictContextProvider";
+import React, { useEffect, useState } from "react";
+import { useQuery } from 'react-query'
+// import { PREDICT_CONTRACT_ADDRESS } from "@/services/constants";
+
+
+type Props = {};
+const CurrentBet = (props: Props) => {
+    const [inputValue, setInputValue] = useState("0");
+    const [inputAddress, setInputAddress] = useState("0");
+    const [inpuId, setInpuId] = useState("0");
+    const [betID, setBetID] = useState("0");
+    const [rewardState, setReward] = useState("0");
+    const [info, setInfo] = useState({
+        id: '0',
+        status: '0',
+        totalUp: '0',
+        totalDown: '0',
+        startTime: '0',
+        endTime: '0',
+        startPrice: '0',
+        upPosition: '0',
+        downPosition: '0',
+        binancePrice: '0',
+        timeStamp: '0'
+    });
+    const [betInfoState, setBetInfo] = useState({
+        upBet: "0",
+        downBet: "0",
+        endPrice: "0",
+        startPrice: "0",
+        totalPrize: "0",
+    });
+    const { data,
+        betInfo,
+        reward,
+        queryBetInfo,
+        queryReward,
+        // startBet,
+        // endBet,
+        upBet,
+        downBet,
+        claimReward,
+        fetchCurrentInfo,
+        // getid
+    } = usePredictStore();
+
+    async function getid() {
+        const addr = "inj1jx9uecvwlf94skkwrfumhv0sjsm85um9mmg9ny";
+        try {
+            // const response = await chainGrpcWasmApi.fetchSmartContractState(
+            //     PREDICT_CONTRACT_ADDRESS,
+            //     toBase64({ current_info: { addr: addr } })
+            // ) as { data: string };
+            // const data = await fromBase64(response.data);
+            // setBetID(parseInt((data.id).toString()).toString());
+            setBetID("0");
+        } catch (e) {
+            setBetID("0");
+        }
+    }
+
+    useEffect(() => {
+        getid();
+    }, []);
+
+    useEffect(() => {
+        setInfo(data);
+        queryReward(betID);
+        setReward(reward);
+    }, [data]);
+
+    useEffect(() => {
+        setBetInfo(betInfo);
+    }, [betInfo]);
+
+
+
+    function handleStartBet() {
+        startBet();
+    }
+    function handleEndBet() {
+        endBet();
+    }
+    function handleUpBet() {
+        upBet(inputValue);
+        setInputValue("0");
+    }
+    function handleDownBet() {
+        downBet(inputValue);
+        setInputValue("0");
+
+    }
+    function handleClaimReward() {
+        claimReward(betID);
+        setReward("0");
+    }
+    function handleQueryBetInfoNext() {
+        queryBetInfo((parseInt(betID, 10) + 1).toString());
+        queryReward((parseInt(betID, 10) + 1).toString());
+        setBetID((parseInt(betID, 10) + 1).toString());
+    }
+    function handleQueryBetInfoPrevious() {
+        queryBetInfo((parseInt(betID, 10) - 1).toString());
+        queryReward((parseInt(betID, 10) - 1).toString());
+        setBetID((parseInt(betID, 10) - 1).toString());
+    }
+    function handleQueryCurrentInfo() {
+        fetchCurrentInfo();
+    }
+    function handleQueryReward() {
+        queryReward(betID as string);
+    }
+    function handleChange(event: any) {
+        let { value, min, max } = event.target;
+
+        if (Number(value) > 1000) {
+            value = 1000;
+        }
+        if (Number(value) < 0.001) {
+            value = 0.001;
+        }
+        setInputValue(value);
+    };
+
+    const Claim = () => {
+        return <button className="--check-and-claim" onClick={handleClaimReward}>Claim Reward</button>
+    }
+    return (
+        <div>
+            <div className="--container-wrapper" >
+                <div className="--container-inner">
+                    <p className="order">
+                        #{info.id} {parseInt(info.endTime) == 0 ? "Pending" : (parseInt(info.endTime) < parseInt(info.timeStamp) ? "Watting for Result" : ("Betting End in " + (parseInt(info.endTime) - parseInt(info.timeStamp)).toString() + ' s'))}
+                    </p>
+                    <div className="line"></div>
+                    <div className="price-tag">
+                        <p>INJ Price</p>
+                        <p className={parseFloat(info.binancePrice) >= parseFloat(info.startPrice) / 1000 ? "price-up" : "price-down"} >${info.binancePrice}</p>
+                        <table className="info">
+                            <tr>
+                                <th className="price-start">Start Price</th>
+                                <th>${parseInt(info.startPrice) / 1000}</th>
+                            </tr>
+                            <tr>
+                                <td className="prize">Total Prize</td>
+                                <td>{(parseFloat(((BigInt(info.totalUp) + BigInt(info.totalDown)) / BigInt("100000000000000")).toString()) / 10000).toString()} $INJ</td>
+                            </tr>
+                            <tr>
+                                <td className="prize">Up Postion</td>
+                                <td>{(parseFloat((BigInt(info.upPosition) / BigInt('10000000000000')).toString()) / 100000).toString()} $INJ</td>
+                            </tr>
+                            <tr>
+                                <td className="prize">Down Postion</td>
+                                <td>{(parseFloat((BigInt(info.downPosition) / BigInt('10000000000000')).toString()) / 100000).toString()} $INJ</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <input type="number" className="input" value={inputValue} onChange={(e) => handleChange(e)} />
+                    <div className="--button-container ---a">
+                        <button onClick={handleUpBet} disabled={info.status != "1"} className="button ---a">UP</button>
+                        {/* <p className="--button-text">UP</p> */}
+                        <object className="--center-vertical" data="up.svg" width="50" height="50"> </object>
+
+                    </div>
+                    <div className="--button-container ---b">
+                        <button onClick={handleDownBet} disabled={info.status != "1"} className="button ---b">DOWN</button>
+                        <object className="--center-vertical" data="down.svg" width="40" height="40"> </object>
+                        {/* <p className="--button-text">DOWN</p> */}
+                    </div>
+                </div>
+            </div >
+
+            <div className="--margin"></div>
+
+            <div className="--container-wrapper -bet--info">
+                <div className="--step">
+                    <div className="--bet-info-id">#{betID}</div>
+                    <a className="previous round">
+                        <button onClick={() => { betID != "0" ? handleQueryBetInfoPrevious() : {} }} className="--change-betId-button">
+                            &#8249;
+                        </button>
+                    </a>
+                    <a className="next round">
+                        <button onClick={handleQueryBetInfoNext} className="--change-betId-button">
+                            &#8250;
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <table className="info">
+                        <tr>
+                            <th className="--bet-info-data">Start Price</th>
+                            <th className="--bet-info-data">${parseInt(betInfoState.startPrice) / 1000}</th>
+                        </tr>
+                        <tr>
+                            <td className="--bet-info-data">End Price</td>
+                            <td className="--bet-info-data">${parseInt(betInfoState.endPrice) / 1000}</td>
+                        </tr>
+                        <tr>
+                            <td className="--bet-info-data">Total Prize</td>
+                            <td className="--bet-info-data">
+                                {parseInt((BigInt(betInfoState.totalPrize as string) / BigInt("10000000000000")).toString()) / 100000} $INJ
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="--bet-info-data">Your Reward</td>
+                            <td className="--bet-info-data">
+                                {parseInt((BigInt(rewardState as string) / BigInt("10000000000000")).toString()) / 100000} $INJ
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div className="--margin">
+
+                </div>
+                <div className=".--bet-info-button">
+                    {/* <button className="--check-and-claim" onClick={handleQueryReward}>CHECK</button> */}
+                    <div>
+                        {rewardState != "0" ? <Claim /> : null}
+                    </div>
+
+                </div>
+            </div>
+        </div >
+
+    );
+};
+
+export default CurrentBet;
---
+++ contexts/PredictContextProvider.tsx
@@ -0,0 +1,306 @@
+// import { PREDICT_CONTRACT_ADDRESS } from "@/services/constants";
+import { chainGrpcWasmApi, msgBroadcastClient } from "@/services/services";
+import { getAddresses } from "@/services/wallet";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+import { useSigningClient } from 'contexts/client'
+
+type StoreState = {
+    data: {
+        id: string;
+        status: string;
+        totalUp: string;
+        totalDown: string;
+        startTime: string;
+        endTime: string;
+        startPrice: string;
+        upPosition: string;
+        downPosition: string;
+        binancePrice: string;
+        timeStamp: string
+
+    },
+    betInfo: {
+        upBet: string,
+        downBet: string,
+        endPrice: string,
+        startPrice: string,
+        totalPrize: string,
+
+    },
+    reward: string,
+    queryBetInfo: (value: string) => void,
+    queryReward: (id: string) => void,
+    // startBet: () => void,
+    // endBet: () => void,
+    upBet: (value: string) => void,
+    downBet: (value: string) => void,
+    claimReward: (value: string) => void,
+    fetchCurrentInfo: () => void,
+    // getid: () => void
+};
+
+const PredictContext = createContext<StoreState>({
+    data: {
+        id: '0',
+        status: '0',
+        totalUp: '0',
+        totalDown: '0',
+        startTime: '0',
+        endTime: '0',
+        startPrice: '0',
+        upPosition: '0',
+        downPosition: '0',
+        binancePrice: '0',
+        timeStamp: '0'
+    },
+    betInfo: {
+        upBet: "0",
+        downBet: "0",
+        endPrice: "0",
+        startPrice: "0",
+        totalPrize: "0",
+
+    },
+    reward: '0',
+    queryBetInfo: (value) => { },
+    queryReward: (id) => { },
+    // startBet: () => { },
+    // endBet: () => { },
+    upBet: (value) => { },
+    downBet: (value) => { },
+    claimReward: (value) => { },
+    fetchCurrentInfo: () => { },
+    // getid: () => { }
+});
+
+export const usePredictStore = () => useContext(PredictContext);
+
+type Props = {
+    children?: React.ReactNode;
+};
+
+const PredictContextProvider = (props: Props) => {
+    const [info, setInfo] = useState({
+        id: '0',
+        status: '0',
+        totalUp: '0',
+        totalDown: '0',
+        startTime: '0',
+        endTime: '0',
+        startPrice: '0',
+        upPosition: '0',
+        downPosition: '0',
+        binancePrice: '0',
+        timeStamp: '0'
+    });
+    const [betInfoState, setBetInfo] = useState({
+        upBet: "0",
+        downBet: "0",
+        endPrice: "0",
+        startPrice: "0",
+        totalPrize: "0",
+    });
+    // const [addr, setAddr] = useState("inj1jx9uecvwlf94skkwrfumhv0sjsm85um9mmg9ny");
+    const [reward, setReward] = useState("0");
+    const { walletAddress, signingClient, coreumQueryClient } = useSigningClient()
+
+    useEffect(() => {
+        const interval = setInterval(() => fetchCurrentInfo(), 5000);
+        return () => clearInterval(interval);
+    }, [walletAddress]);
+
+    const fetchFromBinance = async () => {
+        try {
+            const response = await fetch('https://data.binance.com/api/v3/ticker/price?symbol=INJUSDT');
+            const data = response.json();
+            return data;
+        }
+        catch {
+            return { price: '0' };
+        }
+    };
+
+    async function fetchCurrentInfo() {
+        let binancePrice = await fetchFromBinance();
+        let timeStamp = await queryTimeStamp();
+        let addr = "inj1jx9uecvwlf94skkwrfumhv0sjsm85um9mmg9ny";
+        // if (walletAddress) addr = walletAddress;
+        try {
+            // const response = await chainGrpcWasmApi.fetchSmartContractState(
+            //     PREDICT_CONTRACT_ADDRESS,
+            //     toBase64({ current_info: { addr: addr } })
+            // ) as { data: string };
+
+            // const data = await fromBase64(response.data);
+            // await setInfo({
+            //     id: data.id as string,
+            //     status: data.status as string,
+            //     totalUp: data.totalUp as string,
+            //     totalDown: data.totalDown as string,
+            //     startTime: data.startTime as string,
+            //     endTime: data.endTime as string,
+            //     startPrice: data.startPrice as string,
+            //     upPosition: data.upPosition as string,
+            //     downPosition: data.downPosition as string,
+            //     binancePrice: binancePrice.price,
+            //     timeStamp: timeStamp as string,
+            // });
+            // console.log(addr);
+        } catch (e) {
+            alert((e as any).message);
+        }
+    }
+
+    async function queryTimeStamp() {
+        try {
+            // const response = await chainGrpcWasmApi.fetchSmartContractState(
+            //     PREDICT_CONTRACT_ADDRESS,
+            //     toBase64({ time_stamp_info: {} })
+            // ) as { data: string };
+
+            // const info = fromBase64(response.data);
+            // return info;
+            return '0';
+        } catch (e) {
+            return '0';
+        }
+    }
+
+    async function queryBetInfo(value: string) {
+        try {
+            // const response = await chainGrpcWasmApi.fetchSmartContractState(
+            //     PREDICT_CONTRACT_ADDRESS,
+            //     toBase64({ bet_info: { bet_id: parseInt(value, 10) } })
+            // ) as { data: string };
+
+            // const data = fromBase64(response.data);
+            // setBetInfo({
+            //     upBet: data.upBet as string,
+            //     downBet: data.downBet as string,
+            //     endPrice: data.endPrice as string,
+            //     startPrice: data.startPrice as string,
+            //     totalPrize: data.totalPrize as string,
+            // });
+        } catch (e) {
+            alert(e);
+        }
+    }
+    async function queryReward(id: string) {
+        if (!walletAddress) {
+            return;
+        }
+        try {
+            // const response = await chainGrpcWasmApi.fetchSmartContractState(
+            //     PREDICT_CONTRACT_ADDRESS,
+            //     toBase64({ user_reward: { addr: walletAddress, bet_id: parseInt(id, 10) } })
+            // ) as { data: string };
+            // const desh_data = fromBase64(response.data).toString();
+            // setReward(desh_data);
+        } catch (e) {
+            alert(e)
+        }
+    }
+    async function upBet(value: string) {
+        if (!walletAddress) {
+            alert("No Wallet Connected");
+            return;
+        }
+        const amount = {
+            denom: 'udevcore',
+            amount: BigInt((parseFloat(value) * 1000000000000000000)).toString()
+        }
+
+        try {
+            const msg = MsgExecuteContractCompat.fromJSON({
+                funds: amount,
+                contractAddress: PREDICT_CONTRACT_ADDRESS,
+                sender: walletAddress,
+                msg: {
+                    up_bet: {},
+                },
+            });
+
+            // await msgBroadcastClient.broadcast({
+            //     msgs: msg,
+            //     walletAddress: walletAddress,
+            // });
+            fetchCurrentInfo();
+        } catch (e) {
+            alert((e as any).message);
+        }
+    }
+    async function downBet(value: string) {
+        if (!walletAddress) {
+            alert("No Wallet Connected");
+            return;
+        }
+        const amount = {
+            denom: 'udevcore',
+            amount: BigInt((parseFloat(value) * 1000000000000000000)).toString()
+        }
+
+        try {
+            const msg = MsgExecuteContractCompat.fromJSON({
+                funds: amount,
+                contractAddress: PREDICT_CONTRACT_ADDRESS,
+                sender: walletAddress,
+                msg: {
+                    down_bet: {},
+                },
+            });
+
+            // await msgBroadcastClient.broadcast({
+            //     msgs: msg,
+            //     walletAddress: walletAddress,
+            // });
+            fetchCurrentInfo();
+        } catch (e) {
+            alert((e as any).message);
+        }
+    }
+
+    async function claimReward(value: string) {
+        if (!walletAddress) {
+            alert("No Wallet Connected");
+            return;
+        }
+        try {
+            const msg = MsgExecuteContractCompat.fromJSON({
+                contractAddress: PREDICT_CONTRACT_ADDRESS,
+                sender: walletAddress,
+                msg: {
+                    claim_reward: { bet_id: parseInt(value, 10) },
+                },
+            });
+
+            // await msgBroadcastClient.broadcast({
+            //     msgs: msg,
+            //     walletAddress: walletAddress,
+            // });
+            fetchCurrentInfo();
+        } catch (e) {
+            alert((e as any).message);
+        }
+    }
+
+    return (
+        <PredictContext.Provider
+            value={{
+                data: info,
+                betInfo: betInfoState,
+                reward: reward,
+                queryBetInfo,
+                queryReward,
+                upBet,
+                downBet,
+                claimReward,
+                fetchCurrentInfo,
+                // getid,
+            }}
+        >
+            {props.children}
+        </PredictContext.Provider>
+    );
+};
+export default PredictContextProvider;
