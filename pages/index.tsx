d734c1c2051e59fa041806cd5586a48eacb9210a
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-13 16:04:22 +0700

    remove middle page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,36 +7,36 @@ const Home: NextPage = () => {
   const { walletAddress } = useSigningClient()
 
   return (
-    <WalletLoader>
-      <h1 className="text-6xl font-bold">
-        Welcome to {process.env.NEXT_PUBLIC_CHAIN_NAME} !
-      </h1>
+    // <WalletLoader>
+    //   <h1 className="text-6xl font-bold">
+    //     Welcome to {process.env.NEXT_PUBLIC_CHAIN_NAME} !
+    //   </h1>
 
-      <div className="mt-3 text-2xl">
-        Your wallet address is:{' '}
-        <pre></pre>
-        <Link href={process.env.NEXT_PUBLIC_CHAIN_EXPLORER + "coreum/accounts/" + walletAddress} passHref>
-          <a target="_blank" rel="noreferrer" className="font-mono break-all whitespace-pre-wrap link link-primary">
-            {walletAddress}
-          </a>
-        </Link>
+    //   <div className="mt-3 text-2xl">
+    //     Your wallet address is:{' '}
+    //     <pre></pre>
+    //     <Link href={process.env.NEXT_PUBLIC_CHAIN_EXPLORER + "coreum/accounts/" + walletAddress} passHref>
+    //       <a target="_blank" rel="noreferrer" className="font-mono break-all whitespace-pre-wrap link link-primary">
+    //         {walletAddress}
+    //       </a>
+    //     </Link>
 
-      </div>
+    //   </div>
 
-      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
-        <Link href="/nft" passHref>
-          <a
-            className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
-            <h3 className="text-2xl font-bold">NFT &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Create you NFT class and mint NFTs for it.
-            </p>
-          </a>
-        </Link>
-      </div>
+    //   <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
+    //     <Link href="/nft" passHref>
+    //       <a
+    //         className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
+    //         <h3 className="text-2xl font-bold">NFT &rarr;</h3>
+    //         <p className="mt-4 text-xl">
+    //           Create you NFT class and mint NFTs for it.
+    //         </p>
+    //       </a>
+    //     </Link>
+    //   </div>
 
 
-    </WalletLoader>
+    // </WalletLoader>
   )
 }
 
